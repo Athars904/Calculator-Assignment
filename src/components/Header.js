0832c2cc90667a0d9337ebf690690542387eab1b
Athar Shahzad <Athars904@gmail.com>
2023-12-17 20:01:24 +0500

    Fixed something

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,6 @@ export default function Header() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/calculator">Calculator</Link></li>
-          <li><Link to="/quote">Quote</Link></li>
         </ul>
       </nav>
     </header>
